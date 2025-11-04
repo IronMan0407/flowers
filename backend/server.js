f4ab2c9531a9cbd1b5c2aa56c43938d15423c763
Enkhjin.Gan <enkhjin.gan@tdbm.mn>
2025-11-04 15:08:45 +0800

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,27 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const commentRoutes = require('./routes/comments');
 const postRoutes = require('./routes/posts');
-require('dotenv').config();
-const mongoose = require('mongoose');
+
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
-app.use('/api/comments', commentRoutes);
-app.use('/api/posts', postRoutes);
+app.use(cors({
+  origin: 'https://flowers-six-zeta.vercel.app',
+}));
 
 // Connect to MongoDB
-mongoose.set('debug', true);
-mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://hakuhanna0407_db_user:elB5OJfvhzDxraFi@cluster0.u73rxal.mongodb.net/flowers?retryWrites=true&w=majority&tls=true', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.error('connection error:', err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('connection error:', err));
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
+// Routes
+app.use('/api/comments', commentRoutes);
+app.use('/api/posts', postRoutes);
 
 // Start the server
 const PORT = 5000;
