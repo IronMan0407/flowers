d548920d6e76c2b9fd95eac2a704ea8e8c9c9b56
Enkhjin.Gan <enkhjin.gan@tdbm.mn>
2025-11-05 11:16:33 +0800

    initial commit

--- backend/models/Comment.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const commentSchema = new mongoose.Schema({
-  imageId: String,
-  text: String,
-  timestamp: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Comment', commentSchema);
